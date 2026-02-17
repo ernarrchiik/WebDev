e0ffbe84ab3f9fa32904f63ec031354d0e5e8b55
ernarrchiik <eberenbai701@gmail.com>
2026-02-18 01:07:26 +0500

    Add files via upload

---
+++ LAB3/taks1/JS Fundamentals/operator4.js
@@ -0,0 +1,10 @@
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+
+alert(a + b); 
+
+let ab = prompt("First number?", 1);
+let ba = prompt("Second number?", 2);
+
+alert(Number(a) + Number(b)); 
+
